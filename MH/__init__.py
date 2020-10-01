cbdc75e46eeda3816626615b1e53429549fa585c
Lidia Gianne <lidiagianne@gmail.com>
2020-10-01 00:30:09 -0300

    Delete __init__.py

--- MH/__init__.py
+++
@@ -1,6 +0,0 @@
-# se chamar de um diretorio diferente
-# esse arquivo eh chamado primeiro
-# https://cadernodelaboratorio.com.br/2018/10/01/__init__-py-para-que-serve-este-arquivo/
-
-import sys
-sys.path.append('..')
